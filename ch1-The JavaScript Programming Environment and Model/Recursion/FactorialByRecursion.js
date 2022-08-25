0fb32df2da5f02c817427206dc20868488a9b2e9
shazaAly <shaza.aly@gmail.com>
2022-08-25 03:06:42 +0200

    factorial by recursion

--- ch1-The JavaScript Programming Environment and Model/Recursion/FactorialByRecursion.js
+++ ch1-The JavaScript Programming Environment and Model/Recursion/FactorialByRecursion.js
@@ -1,5 +1,15 @@
+/* When a function is called recursively, the results of the function’s computation are tem‐
+porarily suspended while the recursion is in progress.  */
 
 function factorial(num) {
+  if (num === 1) {
+    return 1
+
+  } else {
+    return num * factorial(num - 1)
+  }
 
 
 }
+let res = factorial(5)
+console.log(res);
